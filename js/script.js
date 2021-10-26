0770af8b0b9aaf47bf6af4697a242ee489715354
LarisaGimbu <gimbularisa@gmail.com>
2021-10-26 15:23:59 +0200

    mail

--- js/script.js
+++ js/script.js
@@ -6,3 +6,32 @@
 // Gioco dei dadi
 // 1 - Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // 2 - Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// console.log('ciao');
+
+const mail = ['alberto@gmail.com', 'giuseppe@gmail.com', 'cristoforo@gmail.com', 'samantha@gmail.com']
+// console.log(mail);
+
+const userMail = 'alberto@gmail.com'; //prompt
+// console.log(userMail);
+
+let mailResults = false;
+// console.log(mailResults)
+
+let textResults = '';
+
+
+for(let i = 0; i < mail.length; i++){
+  if(userMail == mail[i]){
+    mailResults = true
+    // console.log(mailResults)
+    textResults = 'La mail da lei inserita è corretta'
+  }
+}
+if(!(mailResults)){
+  textResults = 'La mail è errata oppure non autorizzata, inserire un\'altra mail';
+}
+
+// console.log(textResults);
+
+const output = document.getElementById('output').innerHTML = textResults;
